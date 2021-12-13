78782347623572a911bc9bfe4d33fcea58c745e3
gmother <gmother@student.21-school.ru>
2021-12-13 17:44:49 +0300

    test 1.0

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -12,18 +12,18 @@ namespace ft
 		class ConstMyIterator;
 		public:
 			//------------------------------------------------------typedef(12/12)---------------------------------------------------------
-			typedef T												value_type;
-			typedef Allocator											allocator_type;
+			typedef T																value_type;
+			typedef Allocator														allocator_type;
 			typedef typename allocator_type::reference								reference;
-			typedef typename allocator_type::const_reference							const_reference;
+			typedef typename allocator_type::const_reference						const_reference;
 			typedef typename allocator_type::pointer								pointer;
-			typedef typename allocator_type::const_pointer								const_pointer;
-			typedef MyIterator											iterator;
-			typedef ConstMyIterator											const_iterator;
+			typedef typename allocator_type::const_pointer							const_pointer;
+			typedef MyIterator														iterator;
+			typedef ConstMyIterator													const_iterator;
 			typedef ft::reverse_iterator<iterator>									reverse_iterator;
-			typedef ft::reverse_iterator<const_iterator>								const_reverse_iterator;
-			typedef std::ptrdiff_t											difference_type;
-			typedef std::size_t											size_type;
+			typedef ft::reverse_iterator<const_iterator>							const_reverse_iterator;
+			typedef std::ptrdiff_t													difference_type;
+			typedef std::size_t														size_type;
 
 		private:
 			allocator_type	_alloc;
